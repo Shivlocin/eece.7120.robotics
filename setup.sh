aa8c5b83bd8afb8346baec6c7f66aa52d060fec6
Jacob Olson <jacob.olson2@gmail.com>
2019-09-25 17:10:23 -0400

    More changes

--- setup.sh
+++ setup.sh
@@ -2,21 +2,29 @@
 
 set -u
 my_loc=$(pwd)
+me=${0##*/}
+echo $me
 
 # This script builds and configures an environment for the fundamentals of 
 # robotics class.
 
 # The variables in this block can be changed to suit the users preference
 
-project_name=robotics_class
+project_name=i"robotics_class"
 project_name_c="Robotics Class"
 
+# The variables below should be altered appropriately once the functionality
+# requiring them is implemented
+
+project_repo="insert repo URL here"
+project_repo_branch="master"
 
 # The variables below here should not be altered unless you know what you're 
 # doing.
 local_host_mount=$HOME/.${project_name}
 config_scripts=false
 sandbox=${my_loc}/sandbox
+project_home=$sandbox/$project_name
 catkin_ws=${my_loc}/catkin_ws
 user=ubuntu
 home=/home/$user
@@ -24,11 +32,21 @@ rebuild=false
 isolate=false
 cmd="/bin/bash"
 
+# This flag blocks off some code to run internal helper scripts which are 
+# currently not required or supported. If, in the future, you wish to use
+# this feature with a git-repo containing helper scripts remove the blocking
+# conditional ("BLOCKING CONDITIONAL") and alter as appropriate,
+config_scripts=false
+# Location of the setup script INSIDE OF THE CONTAINER
+setup_script=""
+
+
 # CLI help and documentation
 options="Available options are:\n"
 options="$options\t -r | --rebuild Rebuild the container.\n"
 options="$options\t -x | --execute Execute the given command in the container.\n"
 options="$options\t -i | --isolate Isolate the containers network.\n"
+options="$options\t -b | --branch  Use the specified branch to build the env.\n"
 
 clihelp="Basic information about this script\n"
 clihelp="$clihelp\t This script constructs a universal environment to aid\n" 
@@ -59,7 +77,7 @@ function checkOS {
 
 function installDocker {
   docker_pkg="docker.io"
-  has_docker=$(dpkg-quert -W --showformat='${Status}\n' $docker_pkg | grep "install ok installed")
+  has_docker=$(dpkg-query -W --showformat='${Status}\n' $docker_pkg | grep "install ok installed")
   if [ -z $has_docker ]; then
     echo
     echo -e "\e[34mInstalling Docker.io\e[0m"
@@ -102,6 +120,16 @@ do
       isolate=true
       shift
       ;;
+    -h|--help)
+      echo $clihelp
+      echo $options
+      shift
+      ;;
+    -b|--branch)
+      project_repo_branch="$2"
+      shift
+      shift
+      ;;
     *)
       echo $options
       exit 1;
@@ -142,7 +170,37 @@ if [[ 2 -gt $(docker images $project_name | wc -l) ]]; then
     echo
     docker build --build-arg UID=$UID -t ${project_name}:base ${my_loc}/docker/
 
-    if
+
+    ####### BLOCKING CONDITIONAL ########
+    ####### BLOCKING CONDITIONAL ########
+    ####### BLOCKING CONDITIONAL ########
+    ####### BLOCKING CONDITIONAL ########
+    if $config_scripts; then
+    ####### BLOCKING CONDITIONAL ########
+    ####### BLOCKING CONDITIONAL ########
+    ####### BLOCKING CONDITIONAL ########
+    ####### BLOCKING CONDITIONAL ########
+    
+    # Check to see if we have the sources
+    if [[ -d $project_home/.git ]]; then
+        # We have the sources. Make sure we have the right version.
+        pushd $project_home
+        current_branch=$(git symbolic-ref --short HEAD)
+        if [[ $current_branch != $project_repo_branch ]]; then
+            echo
+            echo -e "\e[34mChecking out the $project_repo_branch branch.\e[39m"
+            echo
+            git checkout $project_repo_branch || { echo "Failed to checkout the desired branch"; exit 1; }
+        fi
+        popd
+    else
+        # We do not have the sources, get them.
+        echo
+        echo -e "\e[34mCloning The ${project_name_c} Code.\e[39m"
+        echo
+        ## Check out the endor repository into the sandbox
+        git clone --recurse-submodules -b $project_repo_branch $project_repo $project_home
+    fi
     # Run the setup scripts inside the container
     echo
     echo -e "\e[34mRunning the configuration scripts.\e[39m"
@@ -159,14 +217,14 @@ if [[ 2 -gt $(docker images $project_name | wc -l) ]]; then
         --net=host                                                          \
         -t                                                                  \
         -v "${sandbox}:$home/sandbox"                                       \
-        -v "${ws_sandbox_loc}/catkin_ws.${target_arch}:$home/sandbox/catkin_ws" \
+        -v "${catkin_ws}:$home/sandbox/catkin_ws" \
         -v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK):ro           \
         -v $HOME/.ssh:$home/.ssh                                            \
         -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK                                     \
         --user $user                                                        \
         -w $home/sandbox/endor/config/scripts                               \
-        -i "${code_base_name}:base"                                         \
-        /bin/bash -ci "$setup_endor"
+        -i "${project_name}:base"                                         \
+        /bin/bash -ci "$setup_script"
  
 
 
@@ -176,39 +234,115 @@ if [[ 2 -gt $(docker images $project_name | wc -l) ]]; then
     rm -f "${my_loc}/id.cid"
     [[ "$result" -eq "0" ]] || { 
         echo "Failed to create base docker image."; 
-        docker rmi -f millennium:base;
+        docker rmi -f ${project_name}:base;
         exit 1; 
     }
-    docker commit $cid "${code_base_name}:latest"
+    docker commit $cid "${project_name}:latest"
     docker stop $cid && docker rm $cid
-    docker rmi "${code_base_name}:base"
+    docker rmi "${project_name}:base"
 
-    # Provide a convenience executable for opening a millennium shell
+    # Provide a convenience executable for opening a shell
     if [[ ! -d "$HOME/bin" ]]; then
         mkdir "$HOME/bin"
         echo "No bin directory, creating one. You may need to log out before "\
             "the directory is added to your path."
     fi
-    echo "pushd \"$my_loc\" > /dev/null; bash ./millennium \"\$@\"; popd > /dev/null;" > "$HOME/bin/millennium"
-    sudo chmod +x "$HOME/bin/millennium"
+    echo "pushd \"$my_loc\" > /dev/null; bash ./${0##*/} \"\$@\"; popd > /dev/null;" > "$HOME/bin/${project_name}"
+    sudo chmod +x "$HOME/bin/${project_name}"
 
     # Miscellaneous saved data
-    [[ -d $millennium_dir ]] || mkdir $millennium_dir
-    touch "$millennium_dir/bash_history"
-    echo "$endor_repo_branch" > "$millennium_dir/git_branch.txt" 
+    [[ -d $local_host_mount ]] || mkdir $local_host_mount
+    touch "$local_host_mount/bash_history"
+    echo "$project_repo_branch" > "$local_host_mount/git_branch.txt" 
 
-    if [[ "$dontCompile" = true ]]; then
-        # Warn the user that the millennium script did not compile the 
-        # codebase as part of the build process. 
-        echo
-        echo
-        echo -e "\e[5m\e[1m\e[31m  WARNING! WARNING! WARNING! WARNING! \e[0m"
-        echo
-        echo -e "\e[1m    Millennium source code not compiled!"
-        echo -e "   You must run catkin_make in catkin_ws "
-        echo -e " and start millennium with the -isf argument"
-        echo -e "   to finish setting up the environment!\e[0m"
-        echo
-        echo -e "\e[5m\e[1m\e[31m  WARNING! WARNING! WARNING! WARNING! \e[0m"
-    fi
+    ####### BLOCKING CONDITIONAL ########
+    ####### BLOCKING CONDITIONAL ########
+    ####### BLOCKING CONDITIONAL ########
+    ####### BLOCKING CONDITIONAL ########
+    ####### BLOCKING CONDITIONAL ########
   fi
+    ####### BLOCKING CONDITIONAL ########
+    ####### BLOCKING CONDITIONAL ########
+    ####### BLOCKING CONDITIONAL ########
+    ####### BLOCKING CONDITIONAL ########
+    ####### BLOCKING CONDITIONAL ########
+
+                        ################
+                        ###          ###
+                        ###  Launch  ###
+                        ###          ###
+                        ################
+echo
+echo -e "\e[34mStarting The ${code_base_name_c} Docker.\e[39m"
+cat << "EOF"                                          ____
+                                                     /    `.
+                                                    /-----.|          ____
+                                                ___/___.---`--.__.---'    `--.
+                                  _______.-----'           __.--'             )
+                              ,--'---.______________..----'(  __         __.-'
+                                        `---.___,-.|(a (a) /-'  )___.---'
+                                                `-.>------<__.-'
+            ______                       _____..--'      //
+    __.----'      `---._                `._.--._______.-'/))
+,--'---.__              -_                  _.-(`-.____.'// \
+          `-._            `---.________.---'    >\      /<   \
+              \_             `--.___            \ \-__-/ /    \
+                \_                  `----._______\ \  / /__    \
+                  \                      /  |,-------'-'\  `-.__\
+                   \                    (   ||            \      )
+                    `\                   \  ||            /\    /
+                      \                   >-||  @)    @) /\    /
+                      \                  ((_||           \ \_.'|
+                       \                    ||            `-'  |
+                       \                    ||             /   |
+                        \                   ||            (   '|
+                        \                   ||  @)     @)  \   |
+                         \                  ||              \  )
+                          `\_               `|__         ____\ |
+                             \_               | ``----'''     \|
+                               \_              \    .--___    |)
+                                 `-.__          \   |     \   |
+                                      `----.___  \^/|      \/\|
+                                               `--\ \-._  / | |   
+                                                   \ \  `'  \ \
+                                            __...--'  )     (  `-._
+                                           (_        /       `.    `-.__
+                                             `--.__.'          `.       )
+                                                                 `.__.-'
+EOF
+
+# Decide which docker command we are going to use.
+docker_cmd=docker
+if [[ $use_nvidia == 1 ]]; then
+    docker_cmd=nvidia-docker
+fi
+
+network="--net=host"
+mount_tmp="-v /tmp:/tmp"
+
+if $isolate; then
+  network=""
+  mount_tmp=""
+fi
+
+$docker_cmd run                                                         \
+    --rm                                                                \
+    $network                                                            \
+    -t                                                                  \
+    --privileged                                                        \
+    --cap-add=SYS_PTRACE --security-opt seccomp=unconfined              \
+    -v $local_host_mount/bash_history:$home/.bash_history               \
+    -v $local_host_mount/host:$home/host                                \
+    -v /dev/bus/usb:/dev/bus/usb                                        \
+    -v "${sandbox}:$home/sandbox"                                       \
+    -v "${catkin_ws}:$home/sandbox/catkin_ws"                           \
+    -v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK):ro           \
+    -v $HOME/.ssh:$home/.ssh                                            \
+    -v /tmp/.X11-unix:/tmp/.X11-unix                                    \
+    $mount_tmp                                                          \
+    -e DISPLAY=unix$DISPLAY                                             \
+    -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK                                     \
+    --user $user                                                        \
+    -w $home                                                            \
+    -i "${code_base_name}"                                              \
+    /bin/bash -ci "${cmd}"
